7cdb7e01b7267881fabbfc4571d7cb93b44e1035
chrisworkalx <857717575@qq.com>
2022-04-02 14:06:16 +0800

    fix: 修改prettierc文件

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   // 代码结尾是否加分号
-  semi: false,
+  semi: true,
   // 是否使用单引号
   singleQuote: true,
   // 对象大括号内两边是否加空格 { a:0 }
@@ -34,4 +34,4 @@ module.exports = {
   proseWrap: 'preserve',
   // 根据显示样式决定 html 要不要折行
   htmlWhitespaceSensitivity: 'css'
-}
+};
